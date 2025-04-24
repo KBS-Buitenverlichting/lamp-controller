c18f463fc8cbceb9bbe68d5dc744f4f7987e5f42
RobD36 <116569465+RobD36@users.noreply.github.com>
2025-04-24 15:14:40 +0200

    make new files for implementation

---
+++ Projects/Applications/FreeRTOS/FreeRTOS_LoRaWAN/Core/UserMadeFiles/Receive/receive.h
@@ -0,0 +1,16 @@
+/*
+ * receive.h
+ *
+ *  Created on: Apr 24, 2025
+ *      Author: robdr
+ */
+
+#ifndef RECEIVE_RECEIVE_H_
+#define RECEIVE_RECEIVE_H_
+
+#include "LmHandler.h"
+#include "LmHandlerTypes.h"
+
+void Process_Rx_Data(LmHandlerAppData_t *appData, LmHandlerRxParams_t *params);
+
+#endif /* RECEIVE_RECEIVE_H_ */
