6a522767aa51b7245c5d98b5fbb64f39bc591965
RobD36 <116569465+RobD36@users.noreply.github.com>
2025-04-25 10:16:43 +0200

    receive.h

---
+++ Projects/Applications/FreeRTOS/FreeRTOS_LoRaWAN/Core/App/Inc/receive.h
@@ -0,0 +1,13 @@
+/*
+ * receive.h
+ *
+ *  Created on: Apr 24, 2025
+ *      Author: robdr
+ */
+
+#pragma once
+
+#include "LmHandler.h"
+#include "LmHandlerTypes.h"
+
+void Process_Rx_Data(LmHandlerAppData_t *appData, LmHandlerRxParams_t *params);
