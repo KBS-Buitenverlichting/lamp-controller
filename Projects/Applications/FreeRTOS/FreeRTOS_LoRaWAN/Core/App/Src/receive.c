a5384c6eb7438e500c14e360aa8888bb2ca9d2f2
RobD36 <116569465+RobD36@users.noreply.github.com>
2025-05-07 13:59:33 +0200

    LED on/off with commands from TTN

--- Projects/Applications/FreeRTOS/FreeRTOS_LoRaWAN/Core/App/Src/receive.c
+++ Projects/Applications/FreeRTOS/FreeRTOS_LoRaWAN/Core/App/Src/receive.c
@@ -9,4 +9,20 @@ void Process_Rx_Data(const LmHandlerAppData_t *const app_data, const LmHandlerRx
     	APP_LOG(TS_OFF, VLEVEL_M, "%02X ", app_data->Buffer[i]);
     }
     APP_LOG(TS_OFF, VLEVEL_M, "\r\n");
+
+    if (app_data->Buffer[0] == 0xAB)
+    {
+    	APP_LOG(TS_OFF, VLEVEL_M, "Instructions for lamp controller received\n");
+    	switch (app_data->Buffer[1]){
+			case 0x00:
+				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_RESET);
+				APP_LOG(TS_OFF, VLEVEL_M, "Lamp off\n");
+				break;
+			case 0x01:
+				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_SET);
+				APP_LOG(TS_OFF, VLEVEL_M, "Lamp on\n");
+				break;
+    	}
+
+    }
 }
