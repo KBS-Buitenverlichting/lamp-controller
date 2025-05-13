eb3b5d544c3f762fd2c0696a537b91de0e105f0d
RobD36 <116569465+RobD36@users.noreply.github.com>
2025-05-13 14:36:06 +0200

    better printing
    still some data loss

--- Projects/Applications/FreeRTOS/FreeRTOS_LoRaWAN/Core/Src/usart_if.c
+++ Projects/Applications/FreeRTOS/FreeRTOS_LoRaWAN/Core/Src/usart_if.c
@@ -292,9 +292,7 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart2)
   {
 	RxCpltCallback(&charRx, 1, 0);
   }
-  char buffer[32];
-  snprintf(buffer, sizeof(buffer), "Ontvangen: %c\r\n", charRx);
-  vcom_Trace_DMA(buffer, strlen(buffer));
+  vcom_Trace_DMA(&charRx, 1);
   HAL_UART_Receive_IT(huart2, &charRx, 1);
 #else
   if ((NULL != RxCpltCallback) && (HAL_UART_ERROR_NONE == huart2->ErrorCode))
