5e347e6e43211d485bab746e0d135054c9f63307
Wrymouth <jacco.potters@outlook.com>
2025-05-15 12:39:10 +0200

    initialize types

---
+++ Projects/Applications/FreeRTOS/FreeRTOS_LoRaWAN/Core/App/Inc/schedules.h
@@ -0,0 +1,41 @@
+/*
+ * schedules.h
+ *
+ *  Created on: May 15, 2025
+ *      Author: jacco
+ */
+
+#pragma once
+
+// #include "lamp_state.h"
+#include "rtc.h"
+
+#define MAX_SCHEDULE_LIST_LENGTH 10
+
+typedef struct LampConfig {
+	// LampState lamp_state;
+	uint8_t brightness;
+} LampConfig;
+
+typedef struct Schedule {
+	RTC_DateTypeDef start_date;
+	RTC_TimeTypeDef start_time;
+	RTC_DateTypeDef end_date;
+	RTC_TimeTypeDef end_time;
+	LampConfig lamp_config;
+} Schedule;
+
+typedef struct ScheduleNode {
+	Schedule schedule;
+	struct ScheduleNode* next;
+} ScheduleNode;
+
+
+// linked list of schedules
+typedef struct ScheduleList {
+	uint8_t size;
+	ScheduleNode* first;
+} ScheduleList;
+
+
+
---
+++ Projects/Applications/FreeRTOS/FreeRTOS_LoRaWAN/Core/App/Src/schedules.c
@@ -0,0 +1,14 @@
+/*
+ * schedules.c
+ *
+ *  Created on: May 15, 2025
+ *      Author: jacco
+ */
+
+#include "schedules.h"
+
+ScheduleList schedules = {0};
+
+void insertSchedule() {
+	// insert the schedule in the correct place based on start date/time
+};
