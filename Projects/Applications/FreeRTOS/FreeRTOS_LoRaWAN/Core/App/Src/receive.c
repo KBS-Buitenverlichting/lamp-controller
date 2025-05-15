b6499cf275b415593929e5939d14e6c05d006e4a
Bjorn-is-my-name <72093834+Bjorn-is-my-name@users.noreply.github.com>
2025-05-15 13:52:46 +0200

    switch case code implementations broken up into designated functions

--- Projects/Applications/FreeRTOS/FreeRTOS_LoRaWAN/Core/App/Src/receive.c
+++ Projects/Applications/FreeRTOS/FreeRTOS_LoRaWAN/Core/App/Src/receive.c
@@ -14,85 +14,138 @@ void Process_Rx_Data(const LmHandlerAppData_t *const app_data,
 
 void Interpret_Message(const uint8_t *const buffer, const uint8_t buffer_size) {
 	if (buffer_size < MESSAGE_MIN_BYTES) {
-		APP_LOG(TS_OFF, VLEVEL_M, "Message too short to interpret\n");
+		APP_LOG(TS_OFF, VLEVEL_M, "Message too short to interpret\r\n");
 		return;
 	}
 
 	if (buffer[IDENTIFIER_BYTE] == INSTRUCTION_IN) {
 		switch (buffer[SUBTYPE_BYTE]) {
 		case LAMP_OFF:
-			APP_LOG(TS_OFF, VLEVEL_M, "Lamp off\n");
-			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_RESET);
+			Handle_Lamp_Off_Instruction(buffer, buffer_size);
 			break;
 		case LAMP_ON:
-			APP_LOG(TS_OFF, VLEVEL_M, "Lamp on\n");
-			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_SET);
+			Handle_Lamp_On_Instruction(buffer, buffer_size);
 			break;
 		case ACTIVATE_MOTION_SENSOR:
-			APP_LOG(TS_OFF, VLEVEL_M, "Select motion sensor!\n");
+			Handle_Activate_Motion_Sensor_Instruction(buffer, buffer_size);
 			break;
 		case CHANGE_BRIGHTNESS:
-			if (buffer_size < MESSAGE_MIN_BYTES + BRIGHTNESS_PARAMS_BYTE_COUNT) {
-				APP_LOG(TS_OFF, VLEVEL_M,
-						"Brightness command does not include brightness\n");
-			} else {
-				APP_LOG(TS_OFF, VLEVEL_M, "Change brightness to %02X!\n",
-						buffer[PARAMETERS_START_BYTE]);
-			}
+			Handle_Change_Brightness_Instruction(buffer, buffer_size);
 			break;
 		case SEND_BATTERY_STATUS:
-			APP_LOG(TS_OFF, VLEVEL_M, "Report battery state!\r\n");
-			{
-				if (Vrefs_Initialized())
-				{
-					const uint8_t params[] = { SEND_BATTERY_STATUS, Get_Battery_Level() };
-					Tx_Set_Buffer(RESPONSE_OUT_WITH_DATA, RESPONDING_TO_INSTRUCTION, (const uint8_t* const)&params, 2);
-				}
-				else
-				{
-					const uint8_t params[] = { SEND_BATTERY_STATUS, VREFS_NOT_INITIALIZED };
-					Tx_Set_Buffer(RESPONSE_OUT_WITH_DATA, RESPONDING_TO_INSTRUCTION_ERROR, (const uint8_t* const)&params, 2);
-				}
-			}
+			Handle_Send_Battery_Status_Instruction(buffer, buffer_size);
 			break;
-		case SET_BATTERY_VREF:
-			APP_LOG(TS_OFF, VLEVEL_M, "Setting battery min and max vref\r\n");
-			if (buffer_size < MESSAGE_MIN_BYTES + BATTERY_VREF_PARAMS_BYTE_COUNT) {
-				const uint8_t params[] = { SET_BATTERY_VREF };
-				Tx_Set_Buffer(RESPONSE_OUT, MISSING_DATA, (const uint8_t* const)&params, 1);
-			}
-			else
-			{
-				uint16_t min_vref = (buffer[PARAMETERS_START_BYTE] << 8) | buffer[PARAMETERS_START_BYTE + 1];
-				uint16_t max_vref = (buffer[PARAMETERS_START_BYTE + 2] << 8) | buffer[PARAMETERS_START_BYTE + 3];
-				APP_LOG(TS_OFF, VLEVEL_M, "min: %u    max: %u\r\n", min_vref, max_vref);
-				Warning result = Set_Battery_Vref(min_vref, max_vref);
-				if (result == NO_WARNING)
-				{
-					Tx_Set_Ack(SET_BATTERY_VREF);
-				}
-				else
-				{
-					const uint8_t params[] = { SET_BATTERY_VREF, result };
-					Tx_Set_Buffer(RESPONSE_OUT_WITH_DATA, RESPONDING_TO_INSTRUCTION_WARNING, (const uint8_t* const)&params, 2);
-				}
-			}
+		case SET_BATTERY_VREFS:
+			Handle_Set_Battery_Vrefs_Instruction(buffer, buffer_size);
 			break;
 		case SYNCHRONIZE_TIME_AND_DATE:
+			Handle_Synchronize_Time_And_Date_Instruction(buffer, buffer_size);
 			break;
 		case SET_TIMESLOT:
+			Handle_Set_Timeslot_Instruction(buffer, buffer_size);
 			break;
 		case SHOW_TIMETABLE:
-			break;
-		case CHANGE_TIMESLOT:
+			Handle_Show_Timetable_Instruction(buffer, buffer_size);
 			break;
 		case REMOVE_TIMESLOT:
+			Handle_Remove_Timeslot_Instruction(buffer, buffer_size);
 			break;
 		default:
-			APP_LOG(TS_OFF, VLEVEL_M, "Unknown AB command: %02X\n", buffer[SUBTYPE_BYTE]);
+			APP_LOG(TS_OFF, VLEVEL_M, "Unknown AB command: %02X\r\n", buffer[SUBTYPE_BYTE]);
 			break;
 		}
 	} else {
-		APP_LOG(TS_OFF, VLEVEL_M, "Unknown message type: %02X\n", buffer[IDENTIFIER_BYTE]);
+		APP_LOG(TS_OFF, VLEVEL_M, "Unknown message type: %02X\r\n", buffer[IDENTIFIER_BYTE]);
 	}
+}
+
+void Handle_Lamp_Off_Instruction(const uint8_t *const buffer, const uint8_t buffer_size)
+{
+	APP_LOG(TS_OFF, VLEVEL_M, "Lamp off\r\n");
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_RESET);
+}
+
+void Handle_Lamp_On_Instruction(const uint8_t *const buffer, const uint8_t buffer_size)
+{
+	APP_LOG(TS_OFF, VLEVEL_M, "Lamp on\r\n");
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_SET);
+}
+
+void Handle_Activate_Motion_Sensor_Instruction(const uint8_t *const buffer, const uint8_t buffer_size)
+{
+	APP_LOG(TS_OFF, VLEVEL_M, "Select motion sensor\r\n");
+}
+
+void Handle_Change_Brightness_Instruction(const uint8_t *const buffer, const uint8_t buffer_size)
+{
+	if (buffer_size < MESSAGE_MIN_BYTES + BRIGHTNESS_PARAMS_BYTE_COUNT) {
+		APP_LOG(TS_OFF, VLEVEL_M, "Brightness command does not include brightness\n");
+	} else {
+		APP_LOG(TS_OFF, VLEVEL_M, "Change brightness to %02X!\n",
+		buffer[PARAMETERS_START_BYTE]);
+	}
+}
+
+void Handle_Send_Battery_Status_Instruction(const uint8_t *const buffer, const uint8_t buffer_size)
+{
+	APP_LOG(TS_OFF, VLEVEL_M, "Report battery state\r\n");
+
+	if (Vrefs_Initialized())
+	{
+		const uint8_t params[] = { SEND_BATTERY_STATUS, Get_Battery_Level() };
+		Tx_Set_Buffer(RESPONSE_OUT_WITH_DATA, RESPONDING_TO_INSTRUCTION, (const uint8_t* const)&params, sizeof(params));
+	}
+	else
+	{
+		const uint8_t params[] = { SEND_BATTERY_STATUS, VREFS_NOT_INITIALIZED };
+		Tx_Set_Buffer(RESPONSE_OUT_WITH_DATA, RESPONDING_TO_INSTRUCTION_ERROR, (const uint8_t* const)&params, sizeof(params));
+	}
+}
+
+void Handle_Set_Battery_Vrefs_Instruction(const uint8_t *const buffer, const uint8_t buffer_size)
+{
+	APP_LOG(TS_OFF, VLEVEL_M, "Setting battery min and max vref\r\n");
+
+	if (buffer_size < MESSAGE_MIN_BYTES + BATTERY_VREF_PARAMS_BYTE_COUNT)
+	{
+		const uint8_t params[] = { SET_BATTERY_VREFS };
+		Tx_Set_Buffer(RESPONSE_OUT, MISSING_DATA, (const uint8_t* const)&params, sizeof(params));
+		return;
+	}
+
+	uint16_t min_vref = (buffer[PARAMETERS_START_BYTE] << 8) | buffer[PARAMETERS_START_BYTE + 1];
+	uint16_t max_vref = (buffer[PARAMETERS_START_BYTE + 2] << 8) | buffer[PARAMETERS_START_BYTE + 3];
+
+	APP_LOG(TS_OFF, VLEVEL_M, "min: %u    max: %u\r\n", min_vref, max_vref);
+
+	Warning result = Set_Battery_Vref(min_vref, max_vref);
+
+	if (result != NO_WARNING)
+	{
+		const uint8_t params[] = { SET_BATTERY_VREF, result };
+		Tx_Set_Buffer(RESPONSE_OUT_WITH_DATA, RESPONDING_TO_INSTRUCTION_WARNING, (const uint8_t* const)&params, sizeof(params));
+		return;
+	}
+
+	Tx_Set_Ack(SET_BATTERY_VREF);
+}
+
+void Handle_Synchronize_Time_And_Date_Instruction(const uint8_t *const buffer, const uint8_t buffer_size)
+{
+	APP_LOG(TS_OFF, VLEVEL_M, "Sync time and date\r\n");
+}
+
+void Handle_Set_Timeslot_Instruction(const uint8_t *const buffer, const uint8_t buffer_size)
+{
+	APP_LOG(TS_OFF, VLEVEL_M, "Set new timeslot\r\n");
+}
+
+void Handle_Show_Timetable_Instruction(const uint8_t *const buffer, const uint8_t buffer_size)
+{
+	APP_LOG(TS_OFF, VLEVEL_M, "Show timetable\r\n");
+}
+
+void Handle_Remove_Timeslot_Instruction(const uint8_t *const buffer, const uint8_t buffer_size)
+{
+	APP_LOG(TS_OFF, VLEVEL_M, "Remove timeslot\r\n");
 }
