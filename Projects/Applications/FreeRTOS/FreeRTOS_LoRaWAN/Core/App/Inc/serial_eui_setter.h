296f3e96cc53bf58791e4ba7acd73892388082fa
RobD36 <116569465+RobD36@users.noreply.github.com>
2025-05-16 14:53:02 +0200

    add print eui function

--- Projects/Applications/FreeRTOS/FreeRTOS_LoRaWAN/Core/App/Inc/serial_eui_setter.h
+++ Projects/Applications/FreeRTOS/FreeRTOS_LoRaWAN/Core/App/Inc/serial_eui_setter.h
@@ -1,3 +1,9 @@
+/*
+ * serial_eui_setter.h
+ *
+ *  Created on: May 8, 2025
+ *      Author: robdr
+ */
 #pragma once
 
 #include <stdint.h>
@@ -7,7 +13,8 @@
 #include "sys_app.h"
 #include "variable_credentials.h"
 
-#define RX_BUFFER_SIZE 10
+#define RX_BUFFER_SIZE 64
 
 void Print_Rx_Buffer(void);
 void Add_To_Rx_Buffer(uint8_t *rxChar);
+void Print_EUI(const char *label, uint8_t *eui);
--- Projects/Applications/FreeRTOS/FreeRTOS_LoRaWAN/Core/App/Src/serial_eui_setter.c
+++ Projects/Applications/FreeRTOS/FreeRTOS_LoRaWAN/Core/App/Src/serial_eui_setter.c
@@ -1,5 +1,5 @@
 /*
- * serial_interface.c
+ * serial_eui_setter.c
  *
  *  Created on: May 8, 2025
  *      Author: robdr
@@ -22,8 +22,9 @@ void set_joinEUI(uint8_t *EUI) {
 
 void Print_Rx_Buffer(void) {
     if (rx_buffer_index > 0) {
-    	vcom_Trace("Ontvangen: ", 11);
-        vcom_Trace_DMA(rx_buffer, rx_buffer_index);
+    	//vcom_Trace("Ontvangen: ", 11);
+        //vcom_Trace_DMA(rx_buffer, rx_buffer_index);
+    	Print_EUI("Join", joinEUI);
         rx_buffer_index = 0;
     }
 }
@@ -41,3 +42,46 @@ void Add_To_Rx_Buffer(uint8_t *rxChar) {
         rx_buffer_index = 1;
     }
 }
+
+void Print_EUI(const char *label, uint8_t *eui) {
+    char buffer[64];
+    int len = snprintf(buffer, sizeof(buffer),
+                       "%s: %02X%02X%02X%02X%02X%02X%02X%02X\r\n",
+                       label,
+                       eui[0], eui[1], eui[2], eui[3],
+                       eui[4], eui[5], eui[6], eui[7]);
+
+    if (len > 0 && len < sizeof(buffer)) {
+        vcom_Trace_DMA((uint8_t *)buffer, len);
+    } else {
+        const char *error = "Print_EUI: format overflow\r\n";
+        vcom_Trace_DMA((uint8_t *)error, strlen(error));
+    }
+}
+
+/*
+static uint8_t hex2byte(const char *hex) {
+    uint8_t high = (isdigit(hex[0]) ? hex[0] - '0' : toupper(hex[0]) - 'A' + 10);
+    uint8_t low  = (isdigit(hex[1]) ? hex[1] - '0' : toupper(hex[1]) - 'A' + 10);
+    return (high << 4) | low;
+}
+
+void Interperet_Rx_Buffer(void) {
+    // Check of buffer de prefix DEVEUI= heeft (case sensitive)
+    if (rx_buffer_index < 14) return; // minimaal 6 + 16 chars + 1 (newline) ?
+
+    if (strncmp((char *)rx_buffer, "DEVEUI=", 7) == 0) {
+        // Daarna moeten 16 hex tekens volgen (8 bytes)
+        char *hexStr = (char *)(rx_buffer + 7);
+
+        uint8_t newDevEUI[8];
+        for (int i = 0; i < 8; i++) {
+            newDevEUI[i] = hex2byte(&hexStr[i * 2]);
+        }
+        set_devEUI(newDevEUI);
+    }
+
+    // reset buffer na interpretatie
+    rx_buffer_index = 0;
+}
+*/
