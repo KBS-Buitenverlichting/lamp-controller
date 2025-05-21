434ec22d1670ff5588d0b4faa700cb0f05692d8e
Bjorn-is-my-name <72093834+Bjorn-is-my-name@users.noreply.github.com>
2025-05-21 14:48:00 +0200

    show timetable command implementation

--- Projects/Applications/FreeRTOS/FreeRTOS_LoRaWAN/Core/App/Inc/receive.h
+++ Projects/Applications/FreeRTOS/FreeRTOS_LoRaWAN/Core/App/Inc/receive.h
@@ -12,6 +12,7 @@
 #include "LmHandlerTypes.h"
 #include "message_format.h"
 #include "battery.h"
+#include "schedules.h"
 
 void Process_Rx_Data(const LmHandlerAppData_t *const app_data, const LmHandlerRxParams_t *const params);
 void Interpret_Message(const uint8_t *const buffer, const uint8_t buffer_size);
