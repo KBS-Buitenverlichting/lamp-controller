4379f4d90aa2003ba5163237757ba0b3ba245896
Wrymouth <jacco.potters@outlook.com>
2025-06-05 13:52:20 +0200

    mem read

--- Projects/Applications/FreeRTOS/FreeRTOS_LoRaWAN/Core/App/Src/battery.c
+++ Projects/Applications/FreeRTOS/FreeRTOS_LoRaWAN/Core/App/Src/battery.c
@@ -4,6 +4,7 @@
  *  Created on: May 9, 2025
  *      Author: Bjorn Wakker
  */
+#include <stdio.h>
 #include "battery.h"
 
 SemaphoreHandle_t sem_start_battery_read;
@@ -24,13 +25,12 @@ void Start_Get_Battery_Level_Task(void const *argument) {
 		if(xSemaphoreTake(sem_start_battery_read, portMAX_DELAY) != pdPASS) {
 			Error_Handler();
 		}
-		uint8_t send_buf = MAX17048_REG_SOC;
 		// send read request for address
-		if (HAL_I2C_Master_Transmit(&hi2c2, MAX17048_READ_ADDR, &send_buf, 1, HAL_MAX_DELAY) != HAL_OK) {
+		if (HAL_I2C_Mem_Read(&hi2c2, MAX17048_READ_ADDR, MAX17048_REG_SOC, I2C_MEMADD_SIZE_8BIT, (uint8_t*) rx_buffer, BUFFER_SIZE, 100) != HAL_OK) {
 			Error_Handler();
 		}
 		//HAL_I2C_Master_Receive(&hi2c2, MAX17048_READ_ADDR, (uint8_t *)rx_buffer, BUFFER_SIZE,1000); //Receiving in Blocking mode
-		const uint8_t params[] = { SEND_BATTERY_STATUS, rx_buffer[0]};
+		const uint8_t params[] = { SEND_BATTERY_STATUS, rx_buffer[0], rx_buffer[1]};
 		Tx_Set_Buffer(RESPONSE_OUT_WITH_DATA, RESPONDING_TO_INSTRUCTION, (const uint8_t* const)&params, sizeof(params));
 	}
 }
